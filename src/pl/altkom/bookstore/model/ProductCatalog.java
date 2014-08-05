071a57720a989012f72f7746657f7e17d827db1c
Daniel Marczydło <>
2014-08-05 12:13:06 +0200

    Poczatek realizacji projektu - szablon klas

---
+++ src/pl/altkom/bookstore/model/ProductCatalog.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pl.altkom.bookstore.model;
+
+/**
+ *
+ * @author Daniel Marczydło <your.name at your.org>
+ */
+public class ProductCatalog {
+    
+    private String name;
+    private double tax;
+    private double discount; 
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getTax() {
+        return tax;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+}
