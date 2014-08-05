d495adb503ae31c75247c1ab81058681a56215fe
radekozak <>
2014-08-05 12:47:07 +0200

    Adds getters and setters

--- src/pl/altkom/bookstore/model/Person.java
+++ src/pl/altkom/bookstore/model/Person.java
@@ -14,10 +14,64 @@ import java.util.Date;
  * @author Adrian Lapierre <adrian@soft-project.pl>
  */
 public class Person {
-    String firstName;
-    String lastName;
-    int peselNumber;
-    Date dateOfBirth;
-    Kontakt contact;
-    DeliveryAddress address;
+   
+    private String firstName;
+     
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private String lastName;
+    
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private int peselNumber;
+    
+    public int getPeselNumber() {
+        return peselNumber;
+    }
+
+    public void setPeselNumber(int peselNumber) {
+        this.peselNumber = peselNumber;
+    }
+
+    private Date dateOfBirth;
+    
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    
+    private Kontakt contact;
+    
+    public Kontakt getContact() {
+        return contact;
+    }
+
+    public void setContact(Kontakt contact) {
+        this.contact = contact;
+    }
+
+    private DeliveryAddress address;
+    
+    public DeliveryAddress getAddress() {
+        return address;
+    }
+
+    public void setAddress(DeliveryAddress address) {
+        this.address = address;
+    }
 }
